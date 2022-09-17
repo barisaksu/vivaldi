bd9e67ebd808fb22d729e3c67ca284c8c4df2bbe
Barış <baris@barisaksu.com>
2022-09-17 20:59:13 +0300

    add page: Settings

---
+++ src/pages/settings.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+const { t, locale } = useI18n()
+
+const mode = reactive(isDark)
+onMounted(() => {
+  console.log(locale)
+})
+</script>
+
+<template>
+  <div class="grid grid-cols-1">
+    <Select id="locales" v-model:selected="locale" :label="t('button.toggle_langs')">
+      <option value="en">
+        English
+      </option>
+      <option value="tr">
+        Türkçe
+      </option>
+    </Select>
+    <hr class="my-8 h-px bg-gray-200 border-0 dark:bg-gray-700">
+
+    <Checkbox v-model:checked="mode" :label="t('button.toggle_dark')" />
+  </div>
+</template>
+
+<style scoped>
+
+</style>
