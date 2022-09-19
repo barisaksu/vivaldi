18defe88430ee5b880346409b3f1e85b7d2fb6bc
Barış <baris@barisaksu.com>
2022-09-19 23:17:51 +0300

    fix overflow

--- src/layouts/default.vue
+++ src/layouts/default.vue
@@ -1,6 +1,6 @@
 <template>
   <main class="flex h-screen flex-row text-white dark:text-gray-200">
     <Sidebar />
-    <RouterView class="w-full" />
+    <RouterView class="w-full overflow-y-auto" />
   </main>
 </template>
