05c9535ad9216996cc60f5739c48d08f0c945735
Barış <baris@barisaksu.com>
2022-10-23 15:20:42 +0300

    Expose Tailwind colors as CSS custom properties (variables)

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -56,5 +56,23 @@ module.exports = {
   },
   plugins: [
     require('flowbite/plugin'),
+    function ({ addBase, theme }) {
+      function extractColorVars(colorObj, colorGroup = '') {
+        return Object.keys(colorObj).reduce((vars, colorKey) => {
+          const value = colorObj[colorKey]
+
+          const newVars
+            = typeof value === 'string'
+              ? { [`--color${colorGroup}-${colorKey}`]: value }
+              : extractColorVars(value, `-${colorKey}`)
+
+          return { ...vars, ...newVars }
+        }, {})
+      }
+
+      addBase({
+        ':root': extractColorVars(theme('colors')),
+      })
+    },
   ],
 }
