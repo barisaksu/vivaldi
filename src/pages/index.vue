4592ce1da9980d17ba984fb7321a5ba8b33d2a3b
Barış <baris@barisaksu.com>
2022-10-25 00:26:06 +0300

    change logo animation

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -5,19 +5,13 @@ const { t } = useI18n()
 <template>
   <div class="w-full bg-white dark:bg-gray-900">
     <div class="mx-auto max-w-screen-xl py-4 px-4 lg:py-12 lg:px-12">
-      <Transition
-        enter-active-class="animate-pulse"
-        leave-active-class="animate-bounce"
-        appear
+      <h1
+        class="animate__animated animate__jackInTheBox w-min mb-4 bg-gradient-to-br from-blue-400 to-blue-600 bg-clip-text text-4xl font-extrabold leading-none tracking-tight text-transparent md:text-5xl lg:text-6xl"
       >
-        <h1
-          class="mb-4 bg-gradient-to-br from-blue-400 to-blue-600 bg-clip-text text-4xl font-extrabold leading-none tracking-tight text-transparent md:text-5xl lg:text-6xl"
-        >
-          Vivaldi
-        </h1>
-      </Transition>
+        Vivaldi
+      </h1>
       <p
-        class="mb-8 text-lg font-normal text-gray-500 dark:text-gray-400 lg:text-xl"
+        class="mb-8 text-lg font-normal text-gray-500 dark:text-gray-400 lg:text-xl pl-2"
       >
         {{ t('site.shortDescription') }}
       </p>
