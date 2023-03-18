8ecadf2746fddeeab57c6080010160fca2aa40e2
barisaksu <baris@barisaksu.com>
2023-03-18 14:43:22 +0300

    Add border under logo

--- src/components/sidebar/Sidebar.vue
+++ src/components/sidebar/Sidebar.vue
@@ -116,10 +116,10 @@ onMounted(() => {
     </Drawer>
   </Transition>
   <div
-    class="flex h-screen min-w-[3.5rem] flex-col items-center justify-start border-r bg-gray-100 border-gray-200 dark:border-gray-700 dark:bg-gray-800 py-2"
+    class="flex h-screen min-w-[3.5rem] flex-col items-center justify-start border-r bg-gray-100 border-gray-200 dark:border-gray-700 dark:bg-gray-800 pb-2"
   >
     <Transition enter-active-class="animate-pulse" leave-active-class="animate-bounce" appear>
-      <router-link to="/" class="pb-2 md:pb-4">
+      <router-link to="/" class="w-full grid text-center justify-center mb-[10px] pt-[5px] pb-[10px] border-b border-gray-700 bg-[#222e3d]">
         <svg id="Layer_1" class="h-12 w-auto hover:animate-spin" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" width="58px" height="58px" viewBox="0 0 600 600" enable-background="new 0 0 600 600" xml:space="preserve"><path id="color_x5F_2" fill="#235eed" d="M569.139,326.817c0,149.99-120.874,271.701-270.515,273.183    c20.223-8.101,40.297-21.086,58.043-38.823c32.998-33.006,49.898-74.071,48.434-109.127l-0.018-0.183    c-0.593-13.656-100.493-363.756-100.493-363.756s124.012,1.674,98.915,102.054c26.545-27.526,44.919-71.416,3.689-112.89    C502.611,119.901,569.139,215.559,569.139,326.817z" /><path id="color_x5F_1" fill="#3f82f8" d="M223.045,427.555c-47.248,47.238-61.497,110.984-35.989,149.92    C90.303,535.417,22.633,439.04,22.633,326.817c0-122.534,80.698-226.118,191.813-260.778V0l25.271,77.275l0,0l98.881,302.344    C302.234,375.634,258.083,392.508,223.045,427.555z" /></svg>
         <!-- <svg
           class="mt-2 h-6 w-auto hover:animate-spin" width="108" height="69" viewBox="0 0 108 69" fill="none"
